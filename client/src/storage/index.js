0cc1a0715c01d97a6abff3e351187a7f614031f0
Matthew Edge-Williams <mat.edge@gmail.com>
2017-04-11 20:41:42 +1000

    mood pages selectors working

--- client/src/storage/index.js
+++ client/src/storage/index.js
@@ -1,9 +1,11 @@
 const Backbone = require('backbone');
 const eventsData = require('./events.json');
+const myData = require('./mood.json');
 
 function Storage() {
   this.eventsData = new Backbone.Collection();
   this.eventsData.add(eventsData.events);
+  this.myData = myData;
 }
 
 module.exports = new Storage();
